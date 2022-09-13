3f10babfb34af21a4dc6588a7da410051f8f1794
MohamedAliFaidi <dalyyy3259@protonmail.com>
2022-09-13 15:46:40 +0100

    connecting express server to mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-
-
-
-
-
+const express = require("express");
+const db = require("./config/db");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(
+  {
+    origin: true,
+    credentials: true,
+  }
+));
 
+db();
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+  console.log("Server is up on port 3000");
 });
