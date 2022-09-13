88c0a9b4a03a254e0065ff6c891833b88e74aa8a
BahaeddineElkhalfet <102801878+Bahaeddine-GIT@users.noreply.github.com>
2022-09-13 18:16:05 +0100

    add schema for user experience

---
+++ server/Models/experience.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+const expSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  category:{type:String, require:true},
+  location:{type:String,require:true},
+  description:String,
+  rate:Number,
+
+});
+module.exports=mongoose.model("Experience",expSchema)
