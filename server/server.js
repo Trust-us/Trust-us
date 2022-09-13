4068c292992eae246904cd232beb286fca2c7456
MohamedAliFaidi <dalyyy3259@protonmail.com>
2022-09-13 16:21:04 +0100

    design mvc structure server

--- server/server.js
+++ server/server.js
@@ -2,18 +2,26 @@ const express = require("express");
 const db = require("./config/db");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const UsersContoller = require("./Controllers/UsersController")
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-  {
+app.use(
+  cors({
     origin: true,
     credentials: true,
-  }
-));
-
+  })
+);
 db();
+
+
+// routing 
+app.get("/", (req, res) => {
+    res.json({ Hello: "World" });
+  });
+  
+app.post("/signup", UsersContoller.signup);
 
 app.listen(3000, () => {
   console.log("Server is up on port 3000");
