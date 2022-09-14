b0c67337f450da21bc2ba6798442b38b447a76d5
BahaeddineElkhalfet <102801878+Bahaeddine-GIT@users.noreply.github.com>
2022-09-14 15:56:22 +0100

    share form added

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,6 +2,7 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/Experience">Experience</router-link> |
+    <router-link to="/share">Share</router-link> |
     <router-link to="/about">About</router-link> 
 
   </nav>
