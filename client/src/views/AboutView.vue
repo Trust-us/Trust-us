ead892a9bf80531ba669bb5edad98ac7eed9ecfb
mohamedzhioua <zhiouamohamed1989@gmail.com>
2022-09-15 09:10:38 +0100

    added many changes on the frontend delete ,signup ,signin , update ..........

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -3,43 +3,65 @@
 
    <div class="login-page">
       <transition name="fade">
-         <div v-if="!registerActive" class="wallpaper-login"></div>
       </transition>
       <div class="wallpaper-register"></div>
 
       <div class="container">
          <div class="row">
-            <div class="col-lg-4 col-md-6 col-sm-8 mx-auto">
-               <div v-if="!registerActive" class="card login" v-bind:class="{ error: emptyFields }">
-                  <h1>Sign In</h1>
-                  <form class="form-group">
-                     <input v-model="emailLogin" type="email" class="form-control" placeholder="Email" required>
-                     <input v-model="passwordLogin" type="password" class="form-control" placeholder="Password" required>
-                     <input type="submit" class="btn btn-primary" @click="doLogin">
-                     <p>Don't have an account? <a href="#" @click="registerActive = !registerActive, emptyFields = false">Sign up here</a>
-                     </p>
-                  </form>
-               </div>
-
-               <div v-else class="card register" v-bind:class="{ error: emptyFields }">
                   <h1>Sign Up</h1>
                   <form class="form-group">
-                     <input v-model="emailReg" type="email" class="form-control" placeholder="Email" required>
-                     <input v-model="passwordReg" type="password" class="form-control" placeholder="Password" required>
-                     <input v-model="confirmReg" type="password" class="form-control" placeholder="Confirm Password" required>
+                    <input   type="text" class="form-control" placeholder="username" v-model="User.username" required>
+                     <input   type="email" class="form-control" placeholder="Email" v-model="User.email" required>
+                     <input   type="password" class="form-control" placeholder="Password" v-model="User.password" required>
                      <input type="submit" class="btn btn-primary" @click="doRegister">
-                     <p>Already have an account? <a href="#" @click="registerActive = !registerActive, emptyFields = false">Sign in here</a>
+                     <p>Already have an account? <a href="" >Sign in here</a>
                      </p>
                   </form>
                </div>
             </div>
          </div>
 
-      </div>
-   </div>
+      
+  
 
 </div>
 </template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      User: { username:'' ,email: '',  password: '' },
+        input: {
+                    email : "",
+                    password : ""
+                }
+    }
+    
+  },
+   methods: {
+    doRegister() {
+      let newUser = {
+                 username: this.User.username,
+
+        email: this.User.email,
+       password: this.User.password,
+
+      }
+      console.log(newUser);
+      axios.post('http://localhost:3000/signup', newUser)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }
+
+}
+</script>
 <style>
 
 </style>
