dcb75eb13df34080231819b55543bc76fa69e1a8
MohamedAliFaidi <dalyyy3259@protonmail.com>
2022-09-19 18:42:31 +0100

    addin alert when login and sign in

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -6,7 +6,7 @@
                <form class="form-group"  @submit.prevent="doRegister">
                   <input type="text" class="form-control" placeholder="username" v-model="User.username" required>
                   <input type="email" class="form-control" placeholder="Email" v-model="User.email" required>
-                  <input type="password" class="form-control" placeholder="Password" v-model="User.password" required>
+                  <input type="password" class="form-control" placeholder="Password" v-model="User.password" required minlength="8">
                   <input type="submit" class="btn btn-primary" value="register">
                   <p>Already have an account? <a href="/">Sign in here</a>
                   </p>
@@ -46,10 +46,12 @@ export default {
          console.log(newUser);
         await axios.post('http://localhost:3000/signup', newUser ,{withCredentials: true })
             .then(res => {
+               alert("you are registered")
               this.$router.push('/')
                console.log(res)
             })
             .catch((error) => {
+               alert("verify credentials")
                console.log(error);
             })
       }
